974f4cc0de335eb9ba554b79866373861b5f3bd7
tharani1404 <156342859+tharani1404@users.noreply.github.com>
2025-04-11 00:36:07 +0530

    Delete render directory

--- render/backend/routes/askyourneta.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { sendmail, fetchUpdatedResponses } from "../controllers/askyourneta.controller.js";
-
-const router = express.Router();
-router.post("/askquery", sendmail);
-router.post("/getanalytics", fetchUpdatedResponses);
-
-export default router;
